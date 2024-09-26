e49391b0894c66dd4d63a1cbdb0cec165e6c3c2d
Girish14j <giriishdjoshi14@gmail.com>
2024-09-26 16:54:25 +0530

    Update script.js
    Moved keydown event listener inside DOMContentLoaded

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-document.querySelectorAll("i.fa-ellipsis-vertical").forEach((el) => {
-  const parentLink = el.parentNode.closest("a");
-  const link = parentLink.getAttribute("href");
-  console.log(link);
-});
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll("i.fa-ellipsis-vertical").forEach((el) => {
+    const parentLink = el.parentNode.closest("a");
+    const link = parentLink.getAttribute("href");
+    console.log(link);
+  });
 
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape") {
-    document.querySelector(".modal")?.remove();
-  }
-});
-
-document.querySelectorAll("i.fa-ellipsis-vertical").forEach((el) => {
-  el.addEventListener("click", (e) => {
-    e.preventDefault();
-    const modal = document.createElement("div");
-    modal.classList.add(
-      "fixed",
-      "inset-0",
-      "bg-black",
-      "bg-opacity-50",
-      "flex",
-      "items-center",
-      "justify-center",
-      "modal",
-      "z-20",
-      "h-[100%]"
-    );
-    const menu = document.createElement("ul");
-    menu.classList.add("bg-[#2226]", "rounded", "p-2");
-    menu.innerHTML = `
+  document.querySelectorAll("i.fa-ellipsis-vertical").forEach((el) => {
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      const modal = document.createElement("div");
+      modal.classList.add(
+        "fixed",
+        "inset-0",
+        "bg-black",
+        "bg-opacity-50",
+        "flex",
+        "items-center",
+        "justify-center",
+        "modal",
+        "z-20",
+        "h-[100%]"
+      );
+      const menu = document.createElement("ul");
+      menu.classList.add("bg-[#2226]", "rounded", "p-2");
+      menu.innerHTML = `
         <h2> Share The Link! </h2>
         <li><i class="fa-brands fa-x"></i><a href="https://x.com/intent/tweet?text=Checkout " class="modal-share">Share via X</a></li>
         <li><i class="fa-brands fa-snapchat" class="text-[#fdfd69]"></i><a href="snapchat://add" class="modal-share">Share via Snapchat</a></li>
@@ -37,12 +32,20 @@ document.querySelectorAll("i.fa-ellipsis-vertical").forEach((el) => {
         <li><i class="fa-brands fa-whatsapp"></i><a href="https://wa.me/?text=Check This Link!" class="modal-share">Share via WhatsApp</a></li>
         <li><i class="fa-solid fa-share"></i><button class="modal-share" onclick="if (navigator.clipboard) { navigator.clipboard.writeText(window.location.href); this.innerText = 'Copied!'; setTimeout(() => { this.innerText = 'Copy Link'; }, 2000); } else { alert('Your browser does not have support for the Clipboard API'); }">Copy Link</button></li>
       `;
-    modal.appendChild(menu);
-    document.body.appendChild(modal);
-    modal.addEventListener("click", (e) => {
-      if (e.target === modal) {
-        modal.remove();
-      }
+      modal.appendChild(menu);
+      document.body.appendChild(modal);
+      modal.addEventListener("click", (e) => {
+        if (e.target === modal) {
+          modal.remove();
+        }
+      });
     });
+  });
+
+  // CHANGE: Moved keydown event listener inside DOMContentLoaded
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      document.querySelector(".modal")?.remove();
+    }
   });
 });
